e7ed3e764cfb225624e9608c705bcf8fc33e0fc2
josedu90 <jose_edubg@hotmail.com>
2019-12-10 23:24:57 -0500

    update

--- forthewinfc.com/scripts/sync.sh
+++ forthewinfc.com/scripts/sync.sh
@@ -7,7 +7,7 @@
 DEVDIR="web/app/uploads/"
 DEVSITE="http://forthewinfc.test"
 
-PRODDIR="web@167.172.230.52:/srv/www/forthewinfc.com/shared/uploads/"
+PRODDIR="web@167.172.252.62:/srv/www/forthewinfc.com/shared/uploads/"
 PRODSITE="https://josedudev.xyz"
 
 STAGDIR="web@staging.forthewinfc.com:/srv/www/forthewinfc.com/shared/uploads/"
